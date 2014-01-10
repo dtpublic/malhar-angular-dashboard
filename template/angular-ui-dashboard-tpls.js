e80c29f9b0363adba7d2de6add20a67a477d90c0
Nick Holub <nikholub@gmail.com>
2014-01-09 16:07:43 -0800

    AngularJS Dashboard

--- template/angular-ui-dashboard-tpls.js
+++ template/angular-ui-dashboard-tpls.js
@@ -2,24 +2,26 @@ angular.module("ui.dashboard").run(["$templateCache", function($templateCache) {
 
   $templateCache.put("template/dashboard.html",
     "<div>\n" +
-    "    <div class=\"btn-group\">\n" +
-    "        <button type=\"button\" class=\"dropdown-toggle btn btn-primary\" data-toggle=\"dropdown\">Add Widget <span\n" +
-    "                class=\"caret\"></span></button>\n" +
-    "        <ul class=\"dropdown-menu\" role=\"menu\">\n" +
-    "            <li ng-repeat=\"widget in options.widgetDefinitions\">\n" +
-    "                <a href=\"#\" ng-click=\"addWidgetInternal($event, widget);\"><span class=\"label label-primary\">{{widget.directive}}</span></a>\n" +
-    "            </li>\n" +
-    "        </ul>\n" +
-    "    </div>\n" +
+    "    <div class=\"btn-toolbar\">\n" +
+    "        <div class=\"btn-group\" ng-if=\"!options.widgetButtons\">\n" +
+    "            <button type=\"button\" class=\"dropdown-toggle btn btn-primary\" data-toggle=\"dropdown\">Add Widget <span\n" +
+    "                    class=\"caret\"></span></button>\n" +
+    "            <ul class=\"dropdown-menu\" role=\"menu\">\n" +
+    "                <li ng-repeat=\"widget in options.widgetDefinitions\">\n" +
+    "                    <a href=\"#\" ng-click=\"addWidgetInternal($event, widget);\"><span class=\"label label-primary\">{{widget.directive}}</span></a>\n" +
+    "                </li>\n" +
+    "            </ul>\n" +
+    "        </div>\n" +
     "\n" +
-    "    <div class=\"btn-group\">\n" +
-    "    <button ng-repeat=\"widget in options.widgetDefinitions\"\n" +
-    "            ng-click=\"addWidgetInternal($event, widget);\" type=\"button\" class=\"btn btn-primary\">\n" +
-    "        {{widget.directive}}\n" +
-    "    </button>\n" +
-    "    </div>\n" +
+    "        <div class=\"btn-group\" ng-if=\"options.widgetButtons\">\n" +
+    "            <button ng-repeat=\"widget in options.widgetDefinitions\"\n" +
+    "                    ng-click=\"addWidgetInternal($event, widget);\" type=\"button\" class=\"btn btn-primary\">\n" +
+    "                {{widget.directive}}\n" +
+    "            </button>\n" +
+    "        </div>\n" +
     "\n" +
-    "    <button ng-click=\"clear();\" type=\"button\" class=\"btn btn-info\">Clear</button>\n" +
+    "        <button ng-click=\"clear();\" type=\"button\" class=\"btn btn-info\">Clear</button>\n" +
+    "    </div>\n" +
     "\n" +
     "    <div ui-sortable=\"sortableOptions\" ng-model=\"widgets\" class=\"list\">\n" +
     "        <div ng-repeat=\"widget in widgets\" ng-style=\"widget.style\" class=\"widget-container\">\n" +
