fd8fd295276651437fa7c619aa028510d0534ca8
Nick Holub <nikholub@gmail.com>
2014-01-13 17:36:49 -0800

    dashboard dist files

---
+++ dist/angular-ui-dashboard.js
@@ -0,0 +1,143 @@
+'use strict';
+
+angular.module('ui.dashboard', ['ui.bootstrap', 'ui.sortable']);
+
+angular.module('ui.dashboard')
+  .directive('dashboard', function () {
+    return {
+      restrict: 'A',
+      templateUrl: 'template/dashboard.html',
+      scope: true,
+      controller: function ($scope) {
+        $scope.sortableOptions = {
+          stop: function () {
+            //TODO store active widgets in local storage on add/remove/reorder
+            //var titles = _.map($scope.widgets, function (widget) {
+            //  return widget.title;
+            //});
+            //console.log(titles);
+          }
+        };
+      },
+      link: function (scope, element, attrs) {
+        scope.options = scope.$eval(attrs.dashboard);
+
+        var count = 1;
+
+        scope.addWidget = function (widgetDef) {
+          console.log(widgetDef);
+          var widget = {
+            title: 'Widget ' + count++,
+            name: widgetDef.name,
+            attrs: widgetDef.attrs,
+            style: widgetDef.style
+          };
+
+          if (widget.template) {
+            widget.template = widgetDef.template;
+          } else {
+            var directive = widgetDef.directive ? widgetDef.directive : widgetDef.name;
+            widget.directive = directive;
+          }
+
+          scope.widgets.push(widget);
+        };
+
+        scope.removeWidget = function (widget) {
+          scope.widgets.splice(_.indexOf(scope.widgets, widget), 1);
+        };
+
+        scope.clear = function () {
+          scope.widgets = [];
+        };
+
+        scope.widgets = [];
+        _.each(scope.options.defaultWidgets, function (widgetDef) {
+          scope.addWidget(widgetDef);
+        });
+
+        scope.addWidgetInternal = function (event, widgetDef) {
+          event.preventDefault();
+          scope.addWidget(widgetDef);
+        };
+
+        // allow adding widgets externally
+        scope.options.addWidget = scope.addWidget;
+      }
+    };
+  })
+  .directive('widget', ['$compile', function ($compile) {
+    function findWidgetPlaceholder(element) {
+      // widget placeholder is the first (and only) child of .widget-content
+      return angular.element(element.find('.widget-content').children()[0]);
+    }
+
+    return {
+      require: '^dashboard',
+      link: function (scope, element) {
+        var elm = findWidgetPlaceholder(element);
+        var widget = scope.widget;
+
+        if (widget.template) {
+          elm.replaceWith(widget.template);
+          elm = findWidgetPlaceholder(element);
+        } else {
+          elm.attr(widget.directive, '');
+
+          if (widget.attrs) {
+            _.each(widget.attrs, function (value, attr) {
+              elm.attr(attr, value);
+            });
+          }
+        }
+
+
+        $compile(elm)(scope);
+      }
+    };
+  }]);
+angular.module("ui.dashboard").run(["$templateCache", function($templateCache) {
+
+  $templateCache.put("template/dashboard.html",
+    "<div>\n" +
+    "    <div class=\"btn-toolbar\">\n" +
+    "        <div class=\"btn-group\" ng-if=\"!options.widgetButtons\">\n" +
+    "            <button type=\"button\" class=\"dropdown-toggle btn btn-primary\" data-toggle=\"dropdown\">Add Widget <span\n" +
+    "                    class=\"caret\"></span></button>\n" +
+    "            <ul class=\"dropdown-menu\" role=\"menu\">\n" +
+    "                <li ng-repeat=\"widget in options.widgetDefinitions\">\n" +
+    "                    <a href=\"#\" ng-click=\"addWidgetInternal($event, widget);\"><span class=\"label label-primary\">{{widget.name}}</span></a>\n" +
+    "                </li>\n" +
+    "            </ul>\n" +
+    "        </div>\n" +
+    "\n" +
+    "        <div class=\"btn-group\" ng-if=\"options.widgetButtons\">\n" +
+    "            <button ng-repeat=\"widget in options.widgetDefinitions\"\n" +
+    "                    ng-click=\"addWidgetInternal($event, widget);\" type=\"button\" class=\"btn btn-primary\">\n" +
+    "                {{widget.name}}\n" +
+    "            </button>\n" +
+    "        </div>\n" +
+    "\n" +
+    "        <button ng-click=\"clear();\" type=\"button\" class=\"btn btn-info\">Clear</button>\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div ui-sortable=\"sortableOptions\" ng-model=\"widgets\" class=\"dashboard-widget-area\">\n" +
+    "        <div ng-repeat=\"widget in widgets\" ng-style=\"widget.style\" class=\"widget-container\">\n" +
+    "            <div class=\"widget panel panel-default\" widget>\n" +
+    "                <div class=\"widget-header panel-heading\">\n" +
+    "                    <h3 class=\"panel-title\">\n" +
+    "                        {{widget.title}} <span class=\"label label-primary\">{{widget.name}}</span>\n" +
+    "                        <span ng-click=\"removeWidget(widget);\" class=\"glyphicon glyphicon-remove\"></span>\n" +
+    "                    </h3>\n" +
+    "                    \n" +
+    "                </div>\n" +
+    "                <div class=\"widget-content panel-body\">\n" +
+    "                    <div></div>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</div>"
+  );
+
+}]);
