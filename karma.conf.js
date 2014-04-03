ace47f518ce82e21404873ac6ec212bff3e3e592
Nick Holub <nikholub@gmail.com>
2014-04-03 15:37:59 -0700

    PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
