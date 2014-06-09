d4756bdadc854fd6d7cbd35e58ac41ace6944388
Andy Perlitch <andyperlitch@gmail.com>
2014-06-09 12:37:30 -0700

    updated widgetDialogCtrl tests

--- test/spec/widgetDialogCtrl.spec.js
+++ test/spec/widgetDialogCtrl.spec.js
@@ -30,4 +30,33 @@ describe('Controller: WidgetDialogCtrl', function() {
       expect($scope.widget).toEqual(widget);   
     });
 
+    it('should set a default templateUrl if none is supplied in the options', inject(function($rootScope, $controller) {
+      $scope = $rootScope.$new();
+      $controller('WidgetDialogCtrl', {
+          $scope: $scope,
+          $modalInstance: $modalInstance,
+          widget: widget,
+          optionsTemplateUrl: undefined
+      });
+      expect(typeof $scope.optionsTemplateUrl).toEqual('string');
+      expect($scope.optionsTemplateUrl.length > 0).toEqual(true);
+    }));
+
+    describe('the ok method', function() {
+      it('should call close with $scope.result', function() {
+        spyOn($modalInstance, 'close');
+        $scope.ok();
+        expect($modalInstance.close).toHaveBeenCalled();
+        expect($modalInstance.close.calls.argsFor(0)[0] === $scope.result).toEqual(true);
+      });
+    });
+
+    describe('the cancel method', function() {
+      it('should call dismiss', function() {
+        spyOn($modalInstance, 'dismiss');
+        $scope.cancel();
+        expect($modalInstance.dismiss).toHaveBeenCalled();
+      });
+    });
+
 });
