ef79f3cc34fd0a87a9ce704b258a3e48f0e80f6e
Nick Holub <nikholub@gmail.com>
2014-10-07 22:29:49 -0700

    demo app - directives

--- demo/scripts/demo.js
+++ demo/scripts/demo.js
@@ -157,51 +157,6 @@ angular.module('app', [
       $scope.randomValue = Math.random();
     }, 500);
   })
-  .directive('wtTime', function ($interval) {
-    return {
-      restrict: 'A',
-      scope: true,
-      replace: true,
-      template: '<div>Time<div class="alert alert-success">{{time}}</div></div>',
-      link: function (scope) {
-        function update() {
-          scope.time = new Date().toLocaleTimeString();
-        }
-
-        update();
-
-        var promise = $interval(update, 500);
-
-        scope.$on('$destroy', function () {
-          $interval.cancel(promise);
-        });
-      }
-    };
-  })
-  .directive('wtScopeWatch', function () {
-    return {
-      restrict: 'A',
-      replace: true,
-      template: '<div>Value<div class="alert alert-info">{{value}}</div></div>',
-      scope: {
-        value: '=value'
-      }
-    };
-  })
-  .directive('wtFluid', function () {
-    return {
-      restrict: 'A',
-      replace: true,
-      templateUrl: 'template/fluid.html',
-      scope: true,
-      controller: function ($scope) {
-        $scope.$on('widgetResized', function (event, size) {
-          $scope.width = size.width || $scope.width;
-          $scope.height = size.height || $scope.height;
-        });
-      }
-    };
-  })
   .factory('RandomDataModel', function ($interval, WidgetDataModel) {
     function RandomDataModel() {
     }
