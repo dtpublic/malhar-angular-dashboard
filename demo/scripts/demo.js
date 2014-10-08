60126cde39ea4d9563735db2d81487175b475df3
Nick Holub <nikholub@gmail.com>
2014-10-07 22:36:26 -0700

    demo app - data model, explicit save demo

--- demo/scripts/demo.js
+++ demo/scripts/demo.js
@@ -126,7 +126,6 @@ angular.module('app', [
       }
     }
   ])
-
   .controller('DemoCtrl', function ($scope, $interval, $window, widgetDefinitions, defaultWidgets) {
     
     $scope.dashboardOptions = {
@@ -141,59 +140,5 @@ angular.module('app', [
       $scope.randomValue = Math.random();
     }, 500);
 
-  })
-  .controller('ExplicitSaveDemoCtrl', function ($scope, $interval, $window, widgetDefinitions, defaultWidgets) {
-
-    $scope.dashboardOptions = {
-      widgetButtons: true,
-      widgetDefinitions: widgetDefinitions,
-      defaultWidgets: defaultWidgets,
-      storage: $window.localStorage,
-      storageId: 'explicitSave',
-      explicitSave: true
-    };
-    $scope.randomValue = Math.random();
-    $interval(function () {
-      $scope.randomValue = Math.random();
-    }, 500);
-  })
-  .factory('RandomDataModel', function ($interval, WidgetDataModel) {
-    function RandomDataModel() {
-    }
-
-    RandomDataModel.prototype = Object.create(WidgetDataModel.prototype);
-    RandomDataModel.prototype.constructor = WidgetDataModel;
-
-    angular.extend(RandomDataModel.prototype, {
-      init: function () {
-        var dataModelOptions = this.dataModelOptions;
-        this.limit = (dataModelOptions && dataModelOptions.limit) ? dataModelOptions.limit : 100;
-
-        this.updateScope('-');
-        this.startInterval();
-      },
-
-      startInterval: function () {
-        $interval.cancel(this.intervalPromise);
-
-        this.intervalPromise = $interval(function () {
-          var value = Math.floor(Math.random() * this.limit);
-          this.updateScope(value);
-        }.bind(this), 500);
-      },
-
-      updateLimit: function (limit) {
-        this.dataModelOptions = this.dataModelOptions ? this.dataModelOptions : {};
-        this.dataModelOptions.limit = limit;
-        this.limit = limit;
-      },
-
-      destroy: function () {
-        WidgetDataModel.prototype.destroy.call(this);
-        $interval.cancel(this.intervalPromise);
-      }
-    });
-
-    return RandomDataModel;
   });
 
